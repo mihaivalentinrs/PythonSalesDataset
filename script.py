4e595f246204babb179e5a38efd93b13c03b671f
Vasile Valentin Mihai <valentin.mihai.vasile@gmail.com>
2025-11-07 11:17:57 +0200

    Add files via upload

--- script.py
+++ script.py
@@ -2,9 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import xlsxwriter
-import os
-import tarfile
-import urllib
+import seaborn as sns
+
 #github raw url format
 #https://raw.githubusercontent.com/username/repo/branch/path/to/file.xlsx
 
@@ -15,11 +14,10 @@ import urllib
 #VANZARE
 
 def main():
+
     workbook = xlsxwriter.Workbook('ExcelRezultateVanzari.xlsx', {'nan_inf_to_errors': True})
-    worksheet = workbook.add_worksheet()
-    headers = ['Modele', 'Medie vanzare/Model', 'Abatere standard/MedieModel',
-               'An de fabricatie', 'Medie vanzare/An de fabricatie', 'Abatere standard/MedieAnFabricatie',
-               'Echipare', 'Medie vanzare/Echipare', 'Abatere standard/Echipare']
+    worksheet = workbook.add_worksheet('Gestionare - Date')
+    headers = ['Modele', 'Echipare', 'An de fabricatie', 'Medie de vanzare', 'Abatere standard', 'Coeficient de variatie', 'Observatii(Total vanzari)']
     worksheet.write_row('A1', headers)
     dataset = pd.read_csv('raportVanzari.csv', encoding = 'utf-8', encoding_errors = 'ignore')
     if dataset.empty:
@@ -28,91 +26,89 @@ def main():
     data_check = dataset.head()
     print("\nVerificarea incarcarii datelor...", data_check)
 
-    model = dataset['MODEL']
-    versiune  = dataset['VERSIUNE']
-    an_fabricatie = dataset['AN FABRICATIE']
-
-    #Curatare de date, coloana cu vanzari
+     #Curatare de date, coloana cu vanzari
     dataset['MODEL'] = dataset['MODEL'].str.replace(' ','', regex = False)
     dataset['VANZARE'] = dataset['VANZARE'].str.replace(',','', regex=False).str.replace('?','', regex=False).str.replace('lei', '', regex = False).str.strip().astype(float)
 
-    median = dataset['VANZARE'].median()
-    dataset['VANZARE'].fillna(median, inplace = True)
+
+    dataset = dataset.dropna(subset = ['VANZARE'])
     nr_valori_lipsa = dataset['VANZARE'].isnull().sum()
     if nr_valori_lipsa > 0:
-        print("\nEroare la completarea cu mediana!")
+        print("\nEroare la stergerea coloanelor goale.")
         return
+    rezultate_combinate = dataset.groupby(['MODEL', 'VERSIUNE', 'AN FABRICATIE'])['VANZARE'].agg(
+        ['mean', 'std', 'count']).reset_index()
+
+    current_row = 1
+    for index, row in rezultate_combinate.iterrows():
+        worksheet.write(current_row, 0, row['MODEL'])
+        worksheet.write(current_row, 1, row['VERSIUNE'])
+        worksheet.write(current_row, 2, row['AN FABRICATIE'])
+        worksheet.write(current_row, 3, row['mean'])  # Medie Vanzare Combi
+        worksheet.write(current_row, 4, row['std'])  # Abatere Std Combi
+        worksheet.write(current_row, 6, row['count'])  #numarul de observatii non-nule
+        current_row += 1
 
 
-    #Grupare dupa fiecare model, pentru a putea accesa fiecare coloana din fiecare masina in parte
-    #Creează un dicționar cu modelele chei și datele lor ca valori
-    grupare_modele = {model: dataset[dataset['MODEL'] == model] for model in dataset['MODEL'].unique()}
-    grupare_versiune = {versiune: dataset[dataset['VERSIUNE'] == versiune] for versiune in dataset['VERSIUNE'].unique()}
-    grupare_an_fabricatie = {an_fabricatie: dataset[dataset['AN FABRICATIE']==an_fabricatie] for an_fabricatie in dataset['AN FABRICATIE'].unique()}
+    worksheet_plot = workbook.add_worksheet('Vizualizari')
 
-    #Initializarea unor vectori pentru stocarea mediilor fiecarui model/an de fabricatie/versiune pentru a observa diferentele dintre date
-    medii_vanzare_model= []
-    medii_vanzare_an_fabricatie = []
-    medii_vanzare_versiune = []
-    #Vectori de abatere standard
-    std_vanzare_model = []
-    std_vanzare_an_fabricatie = []
-    std_vanzare_versiune = []
-    #Liste pentru modele pentru ca sus avem dictionare
-    lista_modele = []
-    lista_an_fabricatie = []
-    lista_versiune = []
+    grupare_modele_df = dataset.groupby('MODEL')['VANZARE'].mean().reset_index()
+    modele_array = rezultate_combinate['MODEL'].to_numpy()
+    medii_vanzare_array = rezultate_combinate['mean'].to_numpy()
+    grupare_sortata =grupare_modele_df.sort_values(by = 'MODEL', ascending = True)
+    nume_modele_sortate = grupare_sortata['MODEL'].to_numpy()
+    medii_sortate = grupare_sortata['VANZARE'].to_numpy()
 
-    for model in grupare_modele:
-        media = grupare_modele[model]['VANZARE'].mean()
-        abatere_standard_model = grupare_modele[model]['VANZARE'].std()
-        medii_vanzare_model.append(media)
-        std_vanzare_model.append(abatere_standard_model)
-        lista_modele.append(model)
-        print(f"Pentru modelul {model}, media de vanzare este: {media:.2f}.")
+    #1.--Salvarea si generarea Box_plot-ului--
 
-    for row, values in enumerate(lista_modele, start = 1):
-        worksheet.write(row, 0, values)
-    for row, values in enumerate(medii_vanzare_model, start = 1):
-        worksheet.write(row, 1, values)
-    for row, values in enumerate(std_vanzare_model, start = 1):
-        worksheet.write(row, 2, values)
+    plt.figure(figsize=(16, 12))
+    sns.boxplot(x='MODEL', y='mean', data=rezultate_combinate)
+    plt.xticks(rotation=90, ha='right', fontsize=8)
+    plt.title('Distributia Mediei Vanzarilor per Model')
+    plt.ylabel('Medie Vanzare')
+    plt.xlabel('Model')
 
-    print("\n")
-    for an_fabricatie in grupare_an_fabricatie:
-        media = grupare_an_fabricatie[an_fabricatie]['VANZARE'].mean()
-        abatere_standard_an = grupare_an_fabricatie[an_fabricatie]['VANZARE'].std()
-        medii_vanzare_an_fabricatie.append(media)
-        std_vanzare_an_fabricatie.append(abatere_standard_an)
-        lista_an_fabricatie.append(an_fabricatie)
-        print(f"Pentru anul de fabricatie {an_fabricatie}, media de vanzare este: {media:.2f}.")
+    #--Salvare
+    boxplot_filename = 'boxplot.vanzari.png'
+    plt.savefig(boxplot_filename, bbox_inches = 'tight')
+    plt.close()
+    #--Inserarea imaginii in excel
+    worksheet_plot.insert_image('A2', boxplot_filename)
 
-    for row, values in enumerate(lista_an_fabricatie, start = 1):
-        worksheet.write(row, 3, values)
-    for row, values in enumerate(medii_vanzare_an_fabricatie, start = 1):
-        worksheet.write(row, 4, values)
-    for row, values in enumerate(std_vanzare_an_fabricatie, start = 1):
-        worksheet.write(row, 5, values)
+    #2.--Salvarea si generarea Bar Plot-ului
+    plt.figure(figsize = (10,12))
+    plt.barh(modele_array, medii_vanzare_array, label = 'Modele')
+    plt.title("Plot pentru vizualizarea mediilor in functie de model")
+    plt.ylabel("|  Modele  |")
+    plt.xlabel("|  Medii de vanzare  |")
+    plt.legend()
 
-    print("\n")
-    for versiune in grupare_versiune:
-        media = grupare_versiune[versiune]['VANZARE'].mean()
-        abatere_standard_echipare = grupare_versiune[versiune]['VANZARE'].std()
-        medii_vanzare_versiune.append(media)
-        std_vanzare_versiune.append(abatere_standard_echipare)
-        lista_versiune.append(versiune)
-        print(f"Pentru varianta de echipare {versiune}, media de vanzare este: {media:.2f}.")
+    barplot_filename = 'barplot.vanzari.png'
+    plt.savefig(barplot_filename, bbox_inches = 'tight')
+    plt.close()
 
-    for row, values in enumerate(lista_versiune, start = 1):
-        worksheet.write(row, 6, values)
-    for row, values in enumerate(medii_vanzare_versiune,start = 1):
-        worksheet.write(row, 7, values)
-    for row, values in enumerate(std_vanzare_versiune, start = 1):
-        worksheet.write(row, 8, values)
+    worksheet_plot.insert_image('A40', barplot_filename)
 
-    #de continuat scriptul cu functii in dictionarul de date grupare_modele
+    #3.Salvarea si generarea BarPlot-ului - ordonat
+    plt.figure(figsize=(10, 12))
+    plt.barh(nume_modele_sortate, medii_sortate, label='Modele')
+    plt.title("Plot pentru vizualizarea mediilor in functie de model")
+    plt.ylabel("|  Modele  |")
+    plt.xlabel("|  Medii de vanzare  |")
+    plt.legend()
 
+    barplot_ordonat_filename = 'barplot2.vanzari.png'
+    plt.savefig(barplot_ordonat_filename, bbox_inches = 'tight')
+    plt.close()
+
+    worksheet_plot.insert_image('A60', barplot_ordonat_filename)
 
     workbook.close()
+
+
+
+
+
+
 if(__name__ == "__main__"):
     main()
